b2a72916398a7044391a947e8cb0120da12c5bce
Arthur Oresjö <arthur_jul@hotmail.com>
2021-01-14 12:53:39 +0100

    accordion funktinallitet

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,29 +4,62 @@ import styled from "styled-components";
 
 
 const StyledFaq = styled.div` 
-  border: 2 px solid blue;
-  
+  border-bottom: 1px solid #9c9a9a;
+  cursor: crosshair;
+  font-style: oblique;
+   
 `
 ;
+const StyledContainer = styled.div`
+  margin-left: 20px;
+  margin-right: 20px;
+  max-width: 500px;
+`
+const data = [
+  {
+    question:"Varför ska du välja just oss?",
+    answer:"Två riktigt goa gubbar som är lätta att ha och göra med, men även grymma på kod"
+  },
+
+  {
+    question:"Hur lång tid tar det att få en sida gjord?",
+    answer:"Allt beror på hur många 'features' du vill ha som produktägare! Men från någon vecka till en månad! "
+  },
+
+  {
+    question:"Vart ligger erat företag?",
+    answer:"Förtillfället väldigt svävande, Tobbe bor på en ö och jag i en förort"
+  },
+
+  {
+    question:"Vem är coolast?",
+    answer:"Finns inga vem eller du hos oss, allt är VI!"
+  }
+];
 
 const Faq = () => {
-  const [showHide, setShowHide] = useState(false);
+  const [showHide, setShowHide] = useState([]);
 
-  const closeMobileMenu = () => setShowHide(!showHide);
+  //const closeMobileMenu = () => setShowHide(!showHide);
+  const toggleIsOpen = (index) =>{
+    let toggle = [...showHide];
+    toggle[index] = !toggle[index];
+    setShowHide(toggle)
+  }
 
   return (
-    <div className='Content'>
-      <StyledFaq onClick={closeMobileMenu}>
-      <p>lorem question</p>
-      <Collapse isOpened={showHide}><p>lorem answern</p></Collapse>
-      </StyledFaq>
-
-      <StyledFaq onClick={closeMobileMenu}>
-      <p>lorem question</p>
-      <Collapse isOpened={showHide}><p>lorem answer</p></Collapse>
-      </StyledFaq>
+    <StyledContainer>
       
-    </div>
+      <h1>FRÅGOR OCH SVAR</h1>
+      <ul>
+      {data.map((data, index) => (
+      <StyledFaq onClick={() => toggleIsOpen(index)} key={index}>
+      <p>{data.question}</p>
+      <Collapse isOpened={showHide[index]}><p>{data.answer}</p></Collapse>
+      </StyledFaq>
+ ))}
+ </ul>
+    </StyledContainer>
     
 
   );
