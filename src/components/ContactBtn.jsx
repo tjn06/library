52fd26bd34be84fc0ad4a9ae91e1fa9adf6856bf
Arthur Oresjö <arthur_jul@hotmail.com>
2021-01-18 11:22:44 +0100

    logopizza

--- src/components/ContactBtn.jsx
+++ src/components/ContactBtn.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function contactBtn ({left, center, right}, props) {
+function contactBtn (click, props) {
     return(
-<svg width="21" height="23" viewBox="0 0 21 23" fill="none" xmlns="http://www.w3.org/2000/svg"{...props}>
+<svg onclick={click} width="21" height="23" viewBox="0 0 21 23" fill="none" xmlns="http://www.w3.org/2000/svg"{...props}>
 <rect width="5" height="23" fill="red"/>
 <rect x="16" width="5" height="23" fill="red"/>
 <rect x="8" width="5" height="23" fill="red"/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,78 +1,78 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import LogoPizza from './iconcomponents/LogoPizza'
 
 // NavLink is a dependency
-const StyledLink = styled(NavLink)
-` color: blue;
+const StyledHeader = styled.div`
+	border-bottom: 2px solid #969696;
+	height: 100px;
+`
+
+const StyledLink = styled(NavLink)`
+ color: black;
+ text-decoration: none;
 
   &.active {
-    color: red;
+    font-weight: bold;
   }`
 ;
 
+const StyledNav = styled.div`
+	
+	display: flex;
+	flex-direction: row;
+`
 function Header() {
-	function menu() {
-		let navWrapper = document.querySelector(".nav-wrapper"),
-			navToogler = document.querySelector(".nav-toogler");
-		navToogler.addEventListener("click", function(event) {
-			navWrapper.classList.toggle("active");
-		});
-	}
+
 	return (
-		<div className="nav-wrapper" id="topNav" onClick={() => menu()}>
-			<nav className="nav">
-								<div className="logo">
-					{/* <img src={require("../pics/headerlogo.svg")} alt="" /> */}
-				</div>
-				<div className="navigation">
-					<div className="nav-toogler" />
-					<ul className="nav-items">
-						<div className="flex-icons">
+		<StyledHeader >
+			<div> <LogoPizza color="#F1A55F"/></div>
+				<StyledNav>
+
+						<div>
 							{" "}
-              {/* npm fund
-               */}
+             
 							<StyledLink
 								to="/"
 								activeClassName="active"
 								end
 							>
-								Home
+								OM OSS
 							</StyledLink>
 						</div>
-						<div className="flex-icons">
-							{/* <img src={require("../pics/add.svg")} alt="" /> */}
+						<div >
+							
 							<StyledLink
 								to="/menu"
 								activeClassName="active"
 								
 							>
-								Menu
+								MENY
 							</StyledLink>
 						</div>
-						<div className="flex-icons">
-							{/* <img src={require("../pics/stats.svg")} alt="" /> */}
+						<div >
+							
 							<StyledLink
 								to="/faq"
 								activeClassName="active"
 							>
-								Faq
+								FAQ
 							</StyledLink>
 						</div>
-						<div className="flex-icons">
-            {/* <img src={require("../pics/battle.svg")} alt="" /> */}
+						<div >
+            
 							<StyledLink
 								to="/battlemode"
-								// className="toBattlemode"
+								
 								activeClassName="active"
 							>
-								Battlemode
+								HITTA OSS
 							</StyledLink>
 						</div>
-					</ul>
-				</div>
-			</nav>
-		</div>
+						</StyledNav>
+
+		</StyledHeader>
 	);
 }
 
