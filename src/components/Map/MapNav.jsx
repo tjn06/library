a9dbdf68c04ce038dd4b34f41f21189f97d4f495
Arthur Oresjö <arthur_jul@hotmail.com>
2021-01-26 15:59:50 +0100

    some

--- src/components/Map/MapNav.jsx
+++ src/components/Map/MapNav.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import L from 'leaflet';
 import markerIcon from '../../assets/markerIcon.png'
